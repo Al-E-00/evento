277cb8fbe720a0aded1b617f596d897e4024563e
Alessio <alessio.mazzella@siren.io>
2024-02-21 21:29:13 +0100

    Refactor page components and add H1 component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import H1 from '@/components/h1';
 import SearchForm from '@/components/search-form';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-3 pt-36">
-      <h1 className="text-5xl font-bold tracking-tight lg:text-6xl">Find events around you</h1>
+      <H1>Find events around you</H1>
       <p className="mb-12 mt-7 text-2xl opacity-75 lg:text-3xl">
         Browse more than <span className="text-accent font-bold italic underline">10,000 events</span> around you
       </p>
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Logo() {
-  return <Image src="https://bytegrad.com/course-assets/react-nextjs/evento.png" alt="Main logo image" width={53} height={12} />;
+  return (
+    <Link href="/">
+      <Image src="https://bytegrad.com/course-assets/react-nextjs/evento.png" alt="Main logo image" width={53} height={12} />
+    </Link>
+  );
 }
