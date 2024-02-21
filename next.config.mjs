a4b60f5077979167bf0e7279fcf3f1befc47d943
Alessio <alessio.mazzella@siren.io>
2024-02-21 20:35:02 +0100

    Add Prettier configuration and update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bytegrad.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/logo.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image';
+
+export default function Logo() {
+  return <Image src="https://bytegrad.com/course-assets/react-nextjs/evento.png" alt="Main logo image" width={53} height={12} />;
+}
