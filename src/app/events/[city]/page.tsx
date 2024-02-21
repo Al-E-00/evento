0b3dd002d428efe4234b833fd5f863fb6f76dd7b
Alessio <alessio.mazzella@siren.io>
2024-02-21 21:33:09 +0100

    Update styling and layout in [city] page and global CSS

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,7 +2,7 @@ import H1 from '@/components/h1';
 
 export default function City() {
   return (
-    <main>
+    <main className="flex min-h-[110vh] flex-col items-center px-[1.25rem] py-24">
       <H1>Events in Austin</H1>
     </main>
   );
