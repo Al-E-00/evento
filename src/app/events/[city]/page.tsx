43b0a889d9cdde299222bbed7fbdf36d70ddb0c3
Alessio <alessio.mazzella@siren.io>
2024-02-27 19:39:31 +0100

    Add zod package for input validation

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import { Suspense } from 'react';
 import Loading from './loading';
 import FetchEventsData from '@/components/fetch-events-data';
+import { z } from 'zod';
 
 type Props = {
   params: {
@@ -24,14 +25,34 @@ export function generateMetadata({ params }: Props) {
 }
 
 export default async function EventsPage({ params, searchParams }: EventsPageProps) {
-  const city = params.city;
-  const page = searchParams.page || 1;
+  //validate params using zod
+  //do not allow special characters
+  const paramsSchema = z.object({
+    //check that is a valid url containing city names
+    //ex: /^[a-zA-Z-]/
+    city: z.string().refine(city => /^[a-zA-Z\s]+$/.test(decodeURIComponent(city))),
+    page: z.string().optional(),
+  });
+  let city, page;
+
+  try {
+    paramsSchema.parse(params);
+    city = decodeURIComponent(params.city);
+    page = searchParams.page || 1;
+  } catch (e) {
+    return (
+      <div className="flex h-96 flex-col items-center justify-center text-2xl text-red-500">
+        <span>An error occurred</span>
+        <p className="mt-4 text-lg text-gray-500">Invalid parameters. Please check the URL and try again.</p>
+      </div>
+    );
+  }
 
   return (
     <main className="flex min-h-[110vh] flex-col items-center px-[1.25rem] py-24">
       <H1 className="mb-28">
         {city === 'all' && 'All Events'}
-        {city !== 'all' && `Events in ${_.capitalize(city)}`}
+        {city !== 'all' && `Events in ${_.startCase(_.toLower(city))}`}
       </H1>
 
       <Suspense key={city + page} fallback={Loading()}>
