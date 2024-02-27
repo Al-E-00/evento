cdc11b200f9d7f8563eea377a701848c2b4d82d8
Alessio <alessio.mazzella@siren.io>
2024-02-27 20:53:52 +0100

    Add server-only package and update import paths

---
+++ src/lib/server-utils.ts
@@ -0,0 +1,44 @@
+import 'server-only';
+
+import { capitalize } from 'lodash';
+import prisma from './db';
+import { notFound } from 'next/navigation';
+import { unstable_cache } from 'next/cache';
+
+export const getEvents = unstable_cache(async (city: string, page = 1) => {
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city: city === 'all' ? undefined : capitalize(city),
+    },
+    orderBy: {
+      date: 'asc',
+    },
+    take: 6,
+    skip: (page - 1) * 6,
+  });
+
+  const totalCount = await prisma.eventoEvent.count({
+    where: {
+      city: city === 'all' ? undefined : capitalize(city),
+    },
+  });
+
+  return {
+    events,
+    totalCount,
+  };
+});
+
+export const getEvent = unstable_cache(async (slug: string) => {
+  const event = await prisma.eventoEvent.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!event) {
+    return notFound();
+  }
+
+  return event;
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,5 @@
 import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { capitalize } from 'lodash';
-import prisma from './db';
-import { notFound } from 'next/navigation';
-import { unstable_cache } from 'next/cache';
 
 export default function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,41 +8,3 @@ export default function cn(...inputs: ClassValue[]) {
 export async function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-export const getEvents = unstable_cache(async (city: string, page = 1) => {
-  const events = await prisma.eventoEvent.findMany({
-    where: {
-      city: city === 'all' ? undefined : capitalize(city),
-    },
-    orderBy: {
-      date: 'asc',
-    },
-    take: 6,
-    skip: (page - 1) * 6,
-  });
-
-  const totalCount = await prisma.eventoEvent.count({
-    where: {
-      city: city === 'all' ? undefined : capitalize(city),
-    },
-  });
-
-  return {
-    events,
-    totalCount,
-  };
-});
-
-export const getEvent = unstable_cache(async (slug: string) => {
-  const event = await prisma.eventoEvent.findUnique({
-    where: {
-      slug: slug,
-    },
-  });
-
-  if (!event) {
-    return notFound();
-  }
-
-  return event;
-});
