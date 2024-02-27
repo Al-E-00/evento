cba98c01e58ba07457770b3b9c2e4e3d3c5c34ee
Alessio <alessio.mazzella@siren.io>
2024-02-27 21:13:16 +0100

    Add contact us page, ophengraph image, privacy policy page, and terms and conditions page

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -1,3 +1,36 @@
-export default function TermsContitionsPage() {
-  return <main>TermsContitionsPage</main>;
+import React from 'react';
+
+export default function TermsConditionsPage() {
+  return (
+    <div className="prose mx-auto mt-auto flex flex-col gap-3 p-4">
+      <h1 className="text-3xl">Terms and Conditions</h1>
+      <p>
+        Welcome to Corp! These terms and conditions outline the rules and regulations for the use of Corp's Website, located at evento.com.
+      </p>
+      <h2 className="text-2xl">License</h2>
+      <p>
+        Unless otherwise stated, Corp and/or its licensors own the intellectual property rights for all material on evento.com. All
+        intellectual property rights are reserved.
+      </p>
+      <h2 className="text-2xl">Hyperlinking to our Content</h2>
+      <p>The following organizations may link to our Website without prior written approval.</p>
+      <h2 className="text-2xl">iFrames</h2>
+      <p>
+        Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual
+        presentation or appearance of our Website.
+      </p>
+      <h2 className="text-2xl">Your Privacy</h2>
+      <p>Please read Privacy Policy.</p>
+      <h2 className="text-2xl">Reservation of Rights</h2>
+      <p>We reserve the right to request that you remove all links or any particular link to our Website.</p>
+      <h2 className="text-2xl">Removal of links from our website</h2>
+      <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment.</p>
+      <h2 className="text-2xl">Disclaimer</h2>
+      <p>
+        To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website
+        and the use of this website.
+      </p>
+      <span className="italic text-gray-500">This is an example page</span>
+    </div>
+  );
 }
